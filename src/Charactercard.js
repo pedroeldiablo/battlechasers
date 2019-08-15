a52d40125be10f015ab6cbbf2db24285d89f13c3
Peter Williams <pedroeldiablo@yahoo.com>
2019-08-15 10:49:41 +0100

    basic set-up with cra

---
+++ src/Charactercard.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Charactercard extends Component {
+    render(){
+        return (
+            <div className="Charactercard">
+                <h1>Character</h1>
+            </div>
+        )      
+    }
+}
+
+export default Charactercard;
